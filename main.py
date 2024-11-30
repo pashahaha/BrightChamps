bdad0cada6d915cf1fd64bb0e1f2a85262c35cea
pashahaha <irfanjunianpasha8@gmail.com>
2024-11-30 11:22:23 +0700

    This is a module lesson

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ print(y)
 
 print ()
 newmodule.greeting("Irfan")
+print()
+newmodule.age(15)
+
+n1 = int(input("Enter the number 1:"))
+n2 = int(input("Enter the number 2:"))
+
+result = newmodule.sum_numbers(n1, n2)
+print(result)
---
+++ newmodule.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    print(f"Hello, {name}!")
+
+def age(umur):
+    print(f"my age is {umur}")
+
+def sum_numbers(num1, num2):
+    return num1 + num2
